38d8484abe1c4200b41c8a9e34ae0cce8da715f0
iago sardiña <fyrlakith@gmail.com>
2022-03-29 11:20:42 +0200

    Update tech.sh

--- old_sh/tech.sh
+++ old_sh/tech.sh
@@ -9,13 +9,13 @@ echo $topic
 # cp /config/custom_components/Techlife/*.so* /usr/lib
 
 if [ "$2" = "on" ]; then
-	$command -en "\xfa\x23\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x23\xfb" | ./custom_components/Techlife/mosquitto_pub -t $topic -s -h 192.168.1.146 -u mosquito -P mosquito
+	$command -en "\xfa\x23\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x23\xfb" | ./custom_components/Techlife/mosquitto_pub -t $topic -s -h 192.168.1.XX -u user -P pass
 
 elif [ "$2" = "off" ]; then
-	$command -en "\xfa\x24\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x24\xfb" | ./custom_components/Techlife/mosquitto_pub -t $topic -s -h 192.168.1.146 -u mosquito -P mosquito
+	$command -en "\xfa\x24\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x24\xfb" | ./custom_components/Techlife/mosquitto_pub -t $topic -s -h 192.168.1.XX -u user -P pass
 
 elif [ "$2" = "dim" ]; then
-    $command -en "\xfa\x23\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x23\xfb" | ./custom_components/Techlife/mosquitto_pub -t $topic -s -h 192.168.1.146 -u mosquito -P mosquito
+    $command -en "\xfa\x23\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x23\xfb" | ./custom_components/Techlife/mosquitto_pub -t $topic -s -h 192.168.1.XX -u user -P pass
     d=$(expr $3 \* 100)
     prefix='28'
     oneToSix='000000000000'
@@ -26,7 +26,7 @@ elif [ "$2" = "dim" ]; then
     xor=$(python3 -c "print(hex($oneToThirteenMod)[2:].lower())")
     suffix='29'
     k=$(echo $prefix$oneToSix$sevenToTen$elevenToThirteen$xor$suffix | sed 's/../\\x&/g' | tr -d "\n")
-    $command -en $k | ./custom_components/Techlife/mosquitto_pub -t $topic -s -h 192.168.1.146 -u mosquito -P mosquito
+    $command -en $k | ./custom_components/Techlife/mosquitto_pub -t $topic -s -h 192.168.1.XX -u user -P pass
 
 fi
 
